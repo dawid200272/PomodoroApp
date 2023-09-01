a0493d743c45d0eee86c697b22679588ac705289
Dawid Machaj <S59073@student.ath.edu.pl>
2023-09-01 17:30:52 +0200

    Add countdown service
    * Added interface for countdown service.
    * Added implementation for countdown service.

---
+++ PomodoroApp.Avalonia/Services/CountdownService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace PomodoroApp.Avalonia.Services;
+public class CountdownService : ICountdownService
+{
+    private readonly Timer _timer;
+
+    private const int INTERVAL_IN_MILISECONDS = 1000;
+    private const int SECONDS_PER_MINUTE = 60;
+
+    private int _timeLeftInSeconds;
+
+    public CountdownService()
+    {
+        _timer = new Timer(interval: INTERVAL_IN_MILISECONDS);
+
+        _timer.Elapsed += Timer_Elapsed;
+    }
+
+    public int MinutesLeft => _timeLeftInSeconds / SECONDS_PER_MINUTE;
+    public int SecondsLeft => _timeLeftInSeconds % SECONDS_PER_MINUTE;
+
+    public bool IsTimeUp => _timeLeftInSeconds == 0;
+
+    public event Action? Tick;
+
+    public void SetTime(int durationInMinutes)
+    {
+        _timeLeftInSeconds = durationInMinutes * SECONDS_PER_MINUTE;
+    }
+
+    public void StartTimer() => _timer.Start();
+
+    public void StopTimer() => _timer.Stop();
+
+    private void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+    {
+        _timeLeftInSeconds--;
+
+        Tick?.Invoke();
+    }
+
+    public void Dispose()
+    {
+        _timer.Elapsed -= Timer_Elapsed;
+
+        _timer.Dispose();
+    }
+}
---
+++ PomodoroApp.Avalonia/Services/ICountdownService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PomodoroApp.Avalonia.Services;
+public interface ICountdownService : IDisposable
+{
+    bool IsTimeUp { get; }
+    int MinutesLeft { get; }
+    int SecondsLeft { get; }
+
+    event Action? Tick;
+
+    void SetTime(int durationInMinutes);
+    void StartTimer();
+    void StopTimer();
+}
